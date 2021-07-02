a7c53e6b6d60dd2643a3e630bbb024c6582b5308
aasis <45727645+praash47@users.noreply.github.com>
2021-07-02 10:37:53 +0545

    📦CREATE: Initialize django and configure database
    - Configure postgresql database information
    - Initialize django backend
    - Initialize app 'requests'
    - Create two helper classes: Requests and RequestGoogle

---
+++ job-miners-server/requestutils/requests.py
@@ -0,0 +1,15 @@
+import requests
+
+class Requests:
+    def __init__(self, url):
+        self.url = url
+
+    def request_to_url(self):
+        return response
+
+class RequestGoogle (Requests):
+    def __init__(self):
+        super.__init__('www.google.com')
+        self._100urls = []
+
+    def get_100_urls(self):
