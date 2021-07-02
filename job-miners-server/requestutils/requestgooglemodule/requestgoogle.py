7d6122538ea52b6bb3d30dc7b7ef026b9db329f5
aasis <45727645+praash47@users.noreply.github.com>
2021-07-02 13:36:30 +0545

    📦 NEW: Write requestgoogle.py
    - Create googleapis.ini | settings for requestgooglemodule
    - Wrote code without documentations for requestgoogle.py
    - Request google is now capable of reading googleapis.ini.
    - Request google now supports getting 100 urls from google server for a given search_query.

--- job-miners-server/requestutils/requestgooglemodule/requestgoogle.py
+++ job-miners-server/requestutils/requestgooglemodule/requestgoogle.py
@@ -1,7 +1,59 @@
-class RequestGoogle (Requests):
+from googleapiclient.discovery import build
+
+from configparser import ConfigParser
+import time
+
+
+CONFIG = 'job-miners-server/requestutils/requestgooglemodule/googleapis.ini'
+
+
+class RequestGoogle:
     def __init__(self):
-        super.__init__('www.google.com')
         self._100urls = []
+        self.time = time.time()
+
+        # Parser
+        self.parser = ConfigParser()
+        self.parser.read(CONFIG)
 
     def get_100_urls(self):
-        self._100.urls = []
+        self._100urls = self.get_from_api()
+
+        return self._100urls
+
+    def get_from_api(self):
+        api = GoogleAPI(
+
+            api_key=self.parser.get('API', 'api_key'),
+            search_engine_id=self.parser.get('API', 'search_engine_id')
+        )
+        search_results = api.search(
+            search_query=self.parser.get('global', 'search_query')
+        )
+
+        return [item['link'] for item in search_results['items']]
+
+class GoogleAPI:
+    def __init__(self, api_key, search_engine_id):
+        self.API_KEY = api_key
+        self.SEARCH_ENGINE_ID = search_engine_id
+        self.usage_count = 0
+        self.last_used_up_date_time = ''
+
+    def search(self, search_query, num_results=100):
+        try:
+            # Google API usage syntax, available on google search API documentation.
+            resource = build("customsearch", 'v1', developerKey=self.API_KEY).cse()
+            response = resource.list(q=search_query, cx=self.SEARCH_ENGINE_ID).execute()
+            
+            if response:
+                self.usage_count += 1
+                return response
+        except Exception as e:
+            print(e)
+            return None
+
+if __name__ == "__main__":
+    google_request = RequestGoogle() 
+    urls = google_request.get_100_urls()
+    print(urls)
