88bd32455c1c02c1644bc56655011b95aa9f861d
aasis <45727645+praash47@users.noreply.github.com>
2021-07-02 14:04:29 +0545

    🐛FIX: Fix only 10 search results fetched
    - requestgoogle was fetching only 10 searchresults.
    - fix applied and fetches number of results placed in num_lifnk in googleapis.ini
    - added todo: request from 3 APIs in such a way that none get banned.

--- job-miners-server/requestutils/requestgooglemodule/requestgoogle.py
+++ job-miners-server/requestutils/requestgooglemodule/requestgoogle.py
@@ -10,6 +10,7 @@ CONFIG = 'job-miners-server/requestutils/requestgooglemodule/googleapis.ini'
 class RequestGoogle:
     def __init__(self):
         self._100urls = []
+        
         self.time = time.time()
 
         # Parser
@@ -17,6 +18,7 @@ class RequestGoogle:
         self.parser.read(CONFIG)
 
     def get_100_urls(self):
+        # TODO: handle logic such that 100 urls are got using different apis. 
         self._100urls = self.get_from_api()
 
         return self._100urls
@@ -27,11 +29,15 @@ class RequestGoogle:
             api_key=self.parser.get('API', 'api_key'),
             search_engine_id=self.parser.get('API', 'search_engine_id')
         )
-        search_results = api.search(
-            search_query=self.parser.get('global', 'search_query')
-        )
+        search_results = []
+        for i in range(1, self.parser.getint('global', 'num_links'), 10): 
+            search_result = api.search(
+                search_query=self.parser.get('global', 'search_query'),
+                start_index=i
+            )
+            search_results += [item['link'] for item in search_result['items']]
 
-        return [item['link'] for item in search_results['items']]
+        return search_results
 
 class GoogleAPI:
     def __init__(self, api_key, search_engine_id):
@@ -40,11 +46,16 @@ class GoogleAPI:
         self.usage_count = 0
         self.last_used_up_date_time = ''
 
-    def search(self, search_query, num_results=100):
+    def search(self, search_query, num_results=10, start_index=1):
         try:
             # Google API usage syntax, available on google search API documentation.
             resource = build("customsearch", 'v1', developerKey=self.API_KEY).cse()
-            response = resource.list(q=search_query, cx=self.SEARCH_ENGINE_ID).execute()
+            response = resource.list(
+                q=search_query,
+                cx=self.SEARCH_ENGINE_ID,
+                num=num_results,
+                start=start_index
+            ).execute()
             
             if response:
                 self.usage_count += 1
