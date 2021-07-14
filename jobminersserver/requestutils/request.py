599d63fcc55a90d5e3ec338c0c447f6c20ea7af5
kalki <46110281+awcm@users.noreply.github.com>
2021-07-14 17:07:53 +0545

    📦 add check_homepage()
    check_homepage() function checks if the url passed is homepage or not

--- jobminersserver/requestutils/request.py
+++ jobminersserver/requestutils/request.py
@@ -15,8 +15,8 @@ class request:
 	request_html(url)
 		returns html in string from the url passed 
 
-	text_from_html()
-		return text seen in webpage from html string
+	check_homepage(url)
+		returns true if url passed is homepage and has no subdomain
 	'''
 	def __init__(self, url):
 		'''
@@ -27,11 +27,34 @@ class request:
 		'''
 		self.url = url
 
+	def check_homepage(self):
+		'''
+		return true if url is the homepage of the site
+		return false otherwise
+
+		Paramters
+		---------
+		home_page: str
+		home_page: home page of the url
+
+		format: https://merojob.com
+		'''
+		home_page = parse.urlsplit(self.url).scheme + '://' + parse.urlsplit(self.url).netloc
+		if(self.url == home_page):
+			return True
+		else:
+			return False
+
 	def get_homepage(self):
 		'''
 		returns homepage from the extracted url
 
 		eg:-https://unjobs.org from   https://unjobs.org/duty_stations/nepal
+
+		Paramters
+		---------
+		home_page: str
+		homepage of the url in the format https://unjobs.org
 		'''
 		home_page = parse.urlsplit(self.url).scheme + '://' + parse.urlsplit(self.url).netloc
 		print(home_page)
@@ -44,16 +67,21 @@ class request:
 
 		If webpage is not accessible print error message 
 		'''
+		
 		try:
 			html = get(self.url).text
 		except:
 			print("Error getting html from url")
 			return ""
 		return html
-url = "https://merojob.com/"
+
+
+url = "https://globaljobnepal.com.np"
 
 R1 = request(url)
+print(R1.check_homepage())
 R1.get_homepage()
+
 html = R1.request_html()
-print(html)
+
 
